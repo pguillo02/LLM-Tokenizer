c3c7057da8038cc6a9966b6a632cdb9a92726ed2
Pablo Guilló <pabloguilloj@gmail.com>
2026-02-22 20:40:14 +0000

    Implementing tests for swapping_most_common_pair

--- testing.py
+++ testing.py
@@ -10,6 +10,12 @@ class test_utils(unittest.TestCase):
         self.assertEqual(utils.most_common_pair([1,2]),(1,2))
         self.assertEqual(utils.most_common_pair([1,2,3,4,5]), (1,2))
 
+    def test_swapping_most_common_pair(self):
+        self.assertEqual(utils.swapping_most_common_pair([1,2,3,4,1,2], (1,2)), [5,3,4,5])
+        self.assertEqual(utils.swapping_most_common_pair([1,2,1,1,1,1,1,1,1], (1,1)), [1,2,3,3,3,1])
+        self.assertEqual(utils.swapping_most_common_pair([1,2,3,4,5], ()), [1,2,3,4,5])
+        self.assertEqual(utils.swapping_most_common_pair([1], ()), [1])
+        self.assertEqual(utils.swapping_most_common_pair([], ()), [])
 
 if __name__ == "__main__":
 
