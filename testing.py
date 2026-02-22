31bab809fb4d071cec64d986668a5abeb03ee9da
Pablo Guilló <pabloguilloj@gmail.com>
2026-02-22 17:47:44 +0000

    Improving tests for most_common_pair

--- testing.py
+++ testing.py
@@ -4,7 +4,11 @@ import unittest
 class test_utils(unittest.TestCase):
     def test_most_common_pair(self):
         self.assertEqual(utils.most_common_pair([1,2,3,4,1,2]), (1,2))
-        self.assertEqual(utils.most_common_pair([1,2,1,1,1,1,1,1], (1,1)))
+        self.assertEqual(utils.most_common_pair([1,2,1,1,1,1,1,1]), (1,1))
+        self.assertEqual(utils.most_common_pair([]), ())
+        self.assertEqual(utils.most_common_pair([1]), ())
+        self.assertEqual(utils.most_common_pair([1,2]),(1,2))
+        self.assertEqual(utils.most_common_pair([1,2,3,4,5]), (1,2))
 
 
 if __name__ == "__main__":
