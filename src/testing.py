32a4992040e0944876b39d6c4c3bbc28e3ac36a1
Pablo Guilló <pabloguilloj@gmail.com>
2026-02-23 15:51:37 +0000

    Redifining structure

---
+++ src/testing.py
@@ -0,0 +1,22 @@
+import utils
+import unittest
+
+class test_utils(unittest.TestCase):
+    def test_most_common_pair(self):
+        self.assertEqual(utils.most_common_pair([1,2,3,4,1,2]), {(1,2):2, (2,3):1, (3,4):1, (4,1):1})
+        self.assertEqual(utils.most_common_pair([1,2,1,1,1,1,1,1]), {(1,2):1, (2,1):1, (1,1):5})
+        self.assertEqual(utils.most_common_pair([]), ())
+        self.assertEqual(utils.most_common_pair([1]), ())
+        self.assertEqual(utils.most_common_pair([1,2]), {(1,2):1})
+        self.assertEqual(utils.most_common_pair([1,2,3,4,5]), {(1,2):1, (2,3):1, (3,4):1, (4,5):1})
+
+    def test_swapping_most_common_pair(self):
+        self.assertEqual(utils.swapping_most_common_pair([1,2,3,4,1,2], (1,2)), [5,3,4,5])
+        self.assertEqual(utils.swapping_most_common_pair([1,2,1,1,1,1,1,1,1], (1,1)), [1,2,3,3,3,1])
+        self.assertEqual(utils.swapping_most_common_pair([1,2,3,4,5], ()), [1,2,3,4,5])
+        self.assertEqual(utils.swapping_most_common_pair([1], ()), [1])
+        self.assertEqual(utils.swapping_most_common_pair([], ()), [])
+
+if __name__ == "__main__":
+
+    unittest.main()
