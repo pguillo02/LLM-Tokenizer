54d54b9a587300c70f9261810e7c84fca24bea77
Pablo Guilló <pabloguilloj@gmail.com>
2026-02-25 17:38:58 +0000

    Adapting the tokenizer to OOP

--- src/tokenizer_bpe.py
+++ src/tokenizer_bpe.py
@@ -1,9 +1,25 @@
 import utils
 
-text_type: str = """The original  BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes. The iteration ends when no sequences can be found, leaving the target text effectively compressed. Decompression can be performed by reversing this process, querying known placeholder terms against their corresponding denoted sequence, using a lookup table. In the original paper, this lookup table is encoded and stored alongside the compressed text."""
+class BPE_tokenizeer:
+    
+    def __init__(self, merges):
+        self.merges = merges
 
-tokens = list(map(int, text_type.encode("utf-8")))
+    @classmethod
+    def train(cls, training_text, training_iterations):
+        tokens = list(training_text.encode("utf-8"))
+        return cls(utils.training_tokenizer(tokens, training_iterations))
 
-merges = utils.training_tokenizer(tokens, 100)
+    def encoder(self):
+        pass
 
-print(merges)
+    def decoder(self):
+        pass
+
+if __name__ == "__main__":
+
+    text_type: str = """The original  BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes. The iteration ends when no sequences can be found, leaving the target text effectively compressed. Decompression can be performed by reversing this process, querying known placeholder terms against their corresponding denoted sequence, using a lookup table. In the original paper, this lookup table is encoded and stored alongside the compressed text."""
+
+    tok = BPE_tokenizeer.train(text_type, 3)
+
+    print(tok.merges)
